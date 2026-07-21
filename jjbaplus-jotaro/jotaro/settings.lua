87ac30684972d221509887ace62daf381f972835
maxi kallenbach <maxikallenbach260@gmail.com>
2026-07-21 00:40:51 -0300

    release changes

--- jjbaplus-jotaro/jotaro/settings.lua
+++ jjbaplus-jotaro/jotaro/settings.lua
@@ -2,5 +2,5 @@ return {
     ReapplyCostume = true,
     VisibleTarget = false,
     -- Skill1 (Star Platinum: The World): no meter cost / cooldown. Leave false for normal play.
-    FreeSkill1 = true,
+    FreeSkill1 = false,
 }
